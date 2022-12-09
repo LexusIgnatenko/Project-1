c7b6422b97bc97af6dd9d487f91b1b86ffa07561
Yahmice <83534639+Yahmice@users.noreply.github.com>
2022-12-09 14:28:41 +0500

    Update txt_stuff.py

--- txt_stuff.py
+++ txt_stuff.py
@@ -1,3 +1,17 @@
+with open('recepies.txt', encoding='utf-8') as file:
+    cook_book = {}
+    for i in file:
+        recepie_name = i.strip()
+        ingredients_count = file.readline()
+        ingredients = []
+        for p in range(int(ingredients_count)):
+            recepie = file.readline().strip().split(' | ')
+            product, quantity, word = recepie
+            ingredients.append({'product': product, 'quantity': quantity, 'measure': word})
+        file.readline()
+    cook_book[recepie_name] = ingredients
+
+    
 def get_shop_list_by_dishes(person_count, **dishes):
     for p in ingredients:
         if p == dishes:
